7f825b6bde96bbc21ef9fdb33a2b31f1b48b441e
MaxFuhrich <maxfuhrich@gmail.com>
2021-10-21 10:23:47 +0200

    cleanup: remove boolean shouldRun in Main.class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
-    protected static boolean shouldRun;
+
     public static void main(String[] args) {
-        shouldRun = true;
         AlarmDisplay display = new AlarmDisplay();
         Monitor monitor = new Monitor(display);
         monitor.start();
